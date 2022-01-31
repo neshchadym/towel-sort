518800d841dae43c9eb3550471826b16cae4eb66
Vova <70287671+neshchadym@users.noreply.github.com>
2022-01-31 18:06:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+module.exports = function towelSort(matrix) {
+    if (arguments.length === 0 || matrix.length === 0) return [];
 
-// You should implement your task here.
+    const matrixSort = matrix.reduce((accum, current, index) => {
+        if (index % 2) {current.reverse();}
+        return [...accum, ...current];
+    }, []);
 
-module.exports = function towelSort (matrix) {
-  return [];
+    return matrixSort;
 }
